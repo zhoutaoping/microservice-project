76097c91d7a4df49f6dde3f205014717bac18e2a
zhoutaoping <18297604236@163.com>
2018-04-26 23:43:45 +0800

    微服务连接eureka并发起调用，使用ribbon做客户端负载均衡

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/ztp/cloud/controller/MovieController.java
@@ -0,0 +1,22 @@
+package com.ztp.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.ztp.cloud.pojo.User;
+
+@RestController
+public class MovieController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("/movie/{id}")
+	public User findUserById(@PathVariable Long id) {
+		return restTemplate.getForObject("http://microservice-provider-user/example/" + id, User.class);
+	}
+	
+}
